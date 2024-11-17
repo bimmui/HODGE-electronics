5ea0274bbfc0720d3360c2e0c331643a4129edf6
Devan Patel <pateldevanc@outlook.com>
2024-11-17 17:18:45 -0500

    updated pi_serial_write to account for one send/receive cycle

--- ground-station/serial-latency-test/pi_serial_read.py
+++ ground-station/serial-latency-test/pi_serial_read.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 with serial.Serial('/dev/ttyACM0', 9600) as serialController:
-	serialController.write()
+	serialController.write(1);
 	timeSent = time.time();
 	print(serialController.readline())
 	timeRecieved = time.time()
