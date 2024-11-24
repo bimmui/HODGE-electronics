428afe7342eb99ce6febefdde4ea50075ec49e93
Devan Patel <pateldevanc@outlook.com>
2024-11-24 15:50:42 -0500

    runs a loop of 10 attemps to see which one is fastest

--- ground-station/serial-latency-test/pi_serial_read.py
+++ ground-station/serial-latency-test/pi_serial_read.py
@@ -2,17 +2,24 @@ import serial
 import time
 
 attemptsToRun = 10
+delayTimes = []
 
 with serial.Serial('/dev/ttyACM0', 9600) as serialController:
+	time.sleep(5)
 	for i in range(attemptsToRun):
 		
-		time.sleep(5)
-		print(f"Attempt {i + 1}")
+		print(f"Attempt {i + 1}:")
+		time.sleep(1)
 
 		serialController.write(str.encode("Howdy\n", "utf-8"))
 		timeSent = time.time()
 		print(serialController.readline())
 		timeRecieved = time.time()
 
-		print(timeRecieved - timeSent)
-		print()
+		delayTimes.add(timeRecieved - timeSent)
+		print(f"Delay time (s): {delayTimes[i]}\n")
+	
+	timeSum = 0
+	for i in range(delayTimes.length()):
+		timeSum += delayTimes[i]
+	print(f"Average delay time {timeSum / i} seconds")
