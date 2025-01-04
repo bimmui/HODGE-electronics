4c4d826483fda69460208e141ad60b673643e607
Devan Patel <pateldevanc@outlook.com>
2025-01-03 19:58:48 -0500

    Updated shared_memory for remote testing (from other computer on different network

--- ground-station/parsing-and-shared-memory/shared_memory.py
+++ ground-station/parsing-and-shared-memory/shared_memory.py
@@ -80,7 +80,7 @@ class DBHandler:
 
 defaultToken = "jkbT8S2dsoHKx_MaG2A8zoboDJF04mssk-F6-1Vt-GMaAuQzlyakxF0ONJ3HEsitjXqd0NrQN0vvJ8qPnZv6MQ=="
 defaultOrg = "TuftsSEDSRocketry"
-defautlUrl = "http://localhost:8086" #uncomment this value for local testing
+#defautlUrl = "http://localhost:8086" #uncomment this value for local testing
 defaultUrl = "http://192.168.1.181:8086" #uncomment this value if doing remote testing
 defaultBucket = "Test"
 tableName = "Fruit Test 3"
