e2b538a94a76d1644f5eef3972dbcc15eb2bd005
Devan Patel <pateldevanc@outlook.com>
2025-01-04 17:17:22 -0500

    shared memory and db handler split, serial reader changed a bit

--- ground-station/parsing-and-shared-memory/serial_reader.py
+++ ground-station/parsing-and-shared-memory/serial_reader.py
@@ -3,6 +3,7 @@ import db_handler
 
 import serial
 import multiprocessing
+import time
 
 defaultToken = "jkbT8S2dsoHKx_MaG2A8zoboDJF04mssk-F6-1Vt-GMaAuQzlyakxF0ONJ3HEsitjXqd0NrQN0vvJ8qPnZv6MQ=="
 defaultOrg = "TuftsSEDSRocketry"
@@ -15,14 +16,23 @@ fieldNames = ["Favorite", "Least Favorite", "Mid"]
 mem = shared_memory.SharedMemory(3, fieldNames)
 db = db_handler.DBHandler(defaultToken, defaultOrg, defaultUrl, defaultBucket, tableName, fieldNames, mem)
 
-entryStart = "["
-entryEnd = "]"
 valueSeparator = ","
+#\n is the entry separator
+
+def logSharedMemoryToDB():
+	while True:
+		if mem.first.data != None:
+			db.writeToDB(mem.first.data)
+			time.sleep(0.01) #Temporarily here for now
+
+p = multiprocessing.Process(target=logSharedMemoryToDB)
+p.start()
+p.join()
 
 with serial.Serial('/dev/ttyACM0', 57600) as serialController:
 	serialController.flush()
 	while True:
 		addInput = serialController.readline().decode("utf-8").strip()
 		#print(addInput)
-		mem.write(addInput.split(","))
+		mem.write(addInput.split(valueSeparator))
 		print(mem.first.data)
