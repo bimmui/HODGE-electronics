36894946d2f1a733eaecc64c23b4763ca857e1ac
Devan Patel <pateldevanc@outlook.com>
2025-02-26 19:14:07 -0500

    new arduino code

--- ground-station/parsing-and-shared-memory/arduino-data-parser/arduino-data-parser.ino
+++ ground-station/parsing-and-shared-memory/arduino-data-parser/arduino-data-parser.ino
@@ -1,20 +1,24 @@
 
-
+int time = 0;
+int velocity = 20;
+int acceleration = 0;
 
 void setup()
 {
   //Open serial port (USB and wait for connection)
 	Serial.begin(115200);
 	while (!Serial); //Wait for serial port connection
-  Serial.write("apple,banana,brocoli\n");
-  Serial.write("pineapple,turnip,swiss chard\n");
-  Serial.write("carrot,mango,dragonfruit\n");
   
 }
 
 void loop()
 {
-
+  String writeCommand = "";
+  writeCommand = writeCommand + "" + time + "," + velocity + "," + acceleration + "\n";
+  Serial.write(writeCommand.c_str());
+  time++;
+  velocity--;
+  acceleration++;
 }
 //Send Data to the pi
 //data: the data to be written.  "," separates fields, "]" separates entries
