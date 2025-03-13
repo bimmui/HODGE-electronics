74ab166e4994ec7a11659e7ecbe23833d48f2fa4
May <MayflowerC10@gmail.com>
2025-03-13 00:57:19 -0400

    Finish proc macro

--- data-analysis/compress-rs/compressor/src/lib.rs
+++ data-analysis/compress-rs/compressor/src/lib.rs
@@ -1,20 +1,19 @@
-use lazy_static::lazy_static;
-use macros::make_answer;
+use macros::export_concrete;
 
 #[cxx::bridge] 
 mod ffi {
 
     extern "Rust" {
         fn return_five() -> u32;
-        fn return_value() -> u32;
     }
 }
 
-#[make_answer]
+#[export_concrete(concrete = Thinger, wrapper = Compressor, module = CompressorModule)]
 pub trait DoThing: Sync {
     fn return_value(&self) -> u32;
-    fn make() -> Self where Self: Sized;
+    fn make() -> Self;
 }
+
 
 struct Thinger {}
 
@@ -27,14 +26,6 @@ impl Thinger {
 impl DoThing for Thinger {
     fn return_value(&self) -> u32 { 6 }
     fn make() -> Self { Thinger {} }
-}
-
-lazy_static! {
-    static ref THING_DOER: Box<dyn DoThing> = Box::new(Thinger {});
-}
-
-fn return_value() -> u32 {
-    THING_DOER.return_value()
 }
 
 fn return_five() -> u32 {
