2c01da4c3ba20786e410c0a48cda0a1fec5ed2b2
Devan Patel <pateldevanc@outlook.com>
2025-03-30 17:59:04 -0400

    redid field_names

--- ground-station/groundstation-application-python/diagnostic_application_runner.py
+++ ground-station/groundstation-application-python/diagnostic_application_runner.py
@@ -18,7 +18,8 @@ url: str = "http://localhost:8086" #uncomment this value for local testing
 #url: str = "http://192.168.1.181:8086" #uncomment this value if doing remote testing
 bucket: str = "Test With Dash Revamped"
 table_name: str = "Dummy alt and velocity integers"
-field_names: list[str,list[int]] = ["Time", "Altitude", "Velocity", "Acceleration", "Positions","Internal Temp", "External Temp"]
+#field_names: list[str,list[int]] = ["Time", "Altitude", "Velocity", "Acceleration", "Positions","Internal Temp", "External Temp"]
+field_names: list[str] = ["Time", "Velocity", "Acceleration"]
 # "Positions": [[37.7749, -122.4194],], # Check to see if typing has to be different bc its a list
 # Old field_names: list[str] = ["Time", "Velocity"]
 baud_rate: int = 88600
