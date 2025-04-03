c89483aee0395e9f2d949f6397564155da27e03b
May Neelon <mayflowerc10@gmail.com>
2025-04-03 15:23:37 -0400

    Rename folder

--- data-analysis/rust/compress/src/lib.rs
+++ data-analysis/rust/compress/src/lib.rs
@@ -1,3 +1,4 @@
+use ffi::set_pin;
 use lazy_static::lazy_static;
 use macros::export_concrete;
 
@@ -9,6 +10,13 @@ mod ffi {
 
     extern "Rust" {
         fn return_five() -> u32;
+        fn toggle_loop();
+    }
+
+    unsafe extern  "C++" {
+        include!("gpio.h");
+
+        fn set_pin(pin: i32, level: i32);
     }
 }
 
@@ -34,4 +42,15 @@ impl DoThing for Thinger {
 
 fn return_five() -> u32 {
     5
+}
+
+const PIN: i32 = 13;
+
+fn toggle_loop() {
+    let mut level = 0;
+    loop {
+        set_pin(PIN, level);
+        level = level ^ 1;
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
 }
