22f815e327642cfc80320e01b4d1b482ef7ca803
May <MayflowerC10@gmail.com>
2025-05-01 23:14:11 -0400

    Add huffman algorithm

--- flight-computer/src/v2/main/compress/compress/src/bitrep.rs
+++ flight-computer/src/v2/main/compress/compress/src/bitrep.rs
@@ -1,3 +1,4 @@
+#[derive(Clone, PartialEq, Eq)]
 pub struct BitRep {
     bytes: Vec<u8>,
     bits_free: u8,
@@ -34,8 +35,52 @@ impl BitRep {
             self.bytes.last_mut().map(|byte| *byte |= bit << self.bits_free);
         }
         else {
-            self.bytes.push(0b1000000);
+            self.bytes.push(0b0000000 | bit << 7);
             self.bits_free = 7;
         }
+    }
+
+    pub fn push_zero(&self) -> BitRep {
+        let mut br = self.clone();
+        br.write_zero();
+        br
+    }
+
+    pub fn push_one(&self) -> BitRep {
+        let mut br = self.clone();
+        br.write_one();
+        br  
+    }
+
+    pub fn len(&self) -> usize {
+        return 8 * self.bytes.len() - self.bits_free as usize;
+    }
+
+    pub fn get_bit(&self, bit_idx: usize) -> Option<u8> {
+        if bit_idx >= self.len() { return None; }
+        let byte = bit_idx / 8;
+        let bit_offset = bit_idx % 8;
+        let val = (self.bytes[byte] >> (7 - bit_offset)) & 1;
+        return Some(val);
+    }
+
+    pub fn iter<'a>(&'a self) -> BitRepIter<'a> {
+        BitRepIter { br: self, idx: 0 }
+    }
+}
+
+pub struct BitRepIter<'a> {
+    br: &'a BitRep,
+    idx: usize,
+}
+
+impl<'a> Iterator for BitRepIter<'a> {
+    type Item = u8;
+    fn next(&mut self) -> Option<Self::Item> {
+        let val = self.br.get_bit(self.idx);
+        if val.is_some() {
+            self.idx += 1;
+        }
+        return val;
     }
 }
--- flight-computer/src/v2/main/compress/compress/src/huffman.rs
+++ flight-computer/src/v2/main/compress/compress/src/huffman.rs
@@ -1,132 +1,142 @@
-use std::{cmp::Ordering, collections::{BTreeMap, BinaryHeap, HashMap, HashSet}, hash::Hash};
-use super::bitrep::*;
+use std::{collections::{BinaryHeap, HashMap}, hash::Hash};
+use crate::bitrep::BitRep;
 use itertools::Itertools;
 
-enum HuffmanNode<T> {
+enum HuffmanNode<T: Clone + Copy + PartialEq + Eq + Hash> {
     Inner(Box<HuffmanNode<T>>, Box<HuffmanNode<T>>),
-    Leaf(T),
+    Leaf(T)
 }
 
-impl<T> From<Node<T>> for HuffmanNode<T> {
-    fn from(value: Node<T>) -> Self {
-        match value {
-            Node::Inner(l, r, _) => HuffmanNode::Inner(Box::new((*l).into()), Box::new((*r).into())),
-            Node::Leaf(val, _) => HuffmanNode::Leaf(val),
-        }
-    }
+struct DataFreq<T: Clone + Copy + PartialEq + Eq + Hash> {
+    pub data: HuffmanNode<T>,
+    pub freq: u64,
 }
 
-impl<T: Copy> HuffmanNode<T> {
-    pub fn next_value<R: BitRead + ?Sized>(&self, reader: &mut R) -> T {
-        match self {
-            HuffmanNode::Leaf(val) => *val,
-            HuffmanNode::Inner(l, r) => {
-                if reader.read_bit().unwrap() {
-                    r.next_value(reader)
-                } else {
-                    l.next_value(reader)
-                }
-            }
-        }
-    }
-}
-
-pub struct TranslationTable<T: Hash + Copy + Eq> {
-    table: HashMap<T, BitRep>,
-}
-
-impl<T: Hash + Copy + Eq> From<&HuffmanNode<T>> for TranslationTable<T> {
-    fn from(value: &HuffmanNode<T>) -> Self {
-        let mut table = HashMap::new();
-        match value {
-            HuffmanNode::Leaf(val) => {
-                table.insert(*val, BitRep { bytes: Vec::new(), bits_free: 64 });
-            }
-            HuffmanNode::Inner(left, right) => {
-                let left_table: TranslationTable<T> = (&**left).into();
-                for (val, bits) in left_table.table.iter() {
-                    let new_bits = BitRep { bits: bits.bits, len: bits.len + 1 };
-                    table.insert(*val, new_bits);
-                }
-
-                let right_table: TranslationTable<T> = (&**right).into();
-                for (val, bits) in right_table.table.iter() {
-                    let new_bits = BitRep { bits: bits.bits | (1 << bits.len + 1), len: bits.len + 1 };
-                    table.insert(*val, new_bits);
-                }
-            }
-        }
-        TranslationTable { table }
-    }
-}
-
-enum Node<T> {
-    Inner(Box<Node<T>>, Box<Node<T>>, u64),
-    Leaf(T, u64),
-}
-
-impl<T> Node<T> {
-    pub fn get_frequency(&self) -> u64 {
-        match self {
-            Self::Inner(_, _, freq) => *freq,
-            Self::Leaf(_, freq) => *freq,
-        }
-    }
-}
-
-impl<T> Ord for Node<T> {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other.get_frequency().cmp(&self.get_frequency())
-    }
-} 
-
-impl<T> PartialOrd for Node<T> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<T> PartialEq for Node<T> {
+impl<T: Clone + Copy + PartialEq + Eq + Hash> PartialEq for DataFreq<T> {
     fn eq(&self, other: &Self) -> bool {
-        self.cmp(other) == Ordering::Equal
+        self.freq == other.freq
     }
 }
 
-impl<T> Eq for Node<T> {}
+impl<T: Clone + Copy + PartialEq + Eq + Hash> Eq for DataFreq<T> {}
 
-pub fn create_huffman<T: Copy + PartialEq + Eq+ Hash>(data: Vec<(T, u64)>) 
-    -> (HuffmanNode<T>,  TranslationTable<T>)
-{
-    let mut queue = BinaryHeap::from_iter(
-        data.iter()
-        .map(|(data, freq)| {
-            Node::Leaf(*data, *freq)
-        })
-        .sorted()
-        .dedup_by(|this, other| {
-            match this {
-                Node::Leaf(val, _) => {
-                    match other {
-                        Node::Leaf(val_other, _) => {
-                            *val == *val_other
-                        },
-                        _ => panic!("Should only be creating leaf nodes!"),
-                    }
-                },
-                _ => panic!("Should only be creating leaf nodes!"),
+// compares so that 'greater' means lower frequency 
+impl<T: Clone + Copy + PartialEq + Eq + Hash> PartialOrd for DataFreq<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        other.freq.partial_cmp(&self.freq)
+    }
+}
+
+impl<T: Clone + Copy + PartialEq + Eq + Hash> Ord for DataFreq<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.partial_cmp(self).unwrap()
+    }
+}
+
+impl<T: Clone + Copy + PartialEq + Eq + Hash> HuffmanNode<T> {
+    pub fn build(data: Vec<T>) -> HuffmanNode<T> {
+        let mut freqs = data
+            .iter()
+            .unique()
+            .map(|i| (*i, 0))
+            .collect::<HashMap<T, u64>>();
+        data.iter().for_each(|i| *freqs.get_mut(i).unwrap() += 1);
+        let mut queue = BinaryHeap::from(
+            freqs
+            .into_iter()
+            .map(|i| DataFreq { data: HuffmanNode::Leaf(i.0), freq: i.1 })
+            .collect::<Vec<DataFreq<T>>>()
+        );
+
+        while queue.len() > 1 {
+            let first = queue.pop().unwrap();
+            let second = queue.pop().unwrap(); 
+            let top = HuffmanNode::Inner(Box::new(first.data), Box::new(second.data));
+            let top_data = DataFreq { data: top, freq: first.freq + second.freq };
+            queue.push(top_data);
+        }
+
+        return queue.pop().unwrap().data;
+    }
+}
+
+type TranslationTable<T: Clone + Copy + PartialEq + Eq + Hash> = HashMap<T, BitRep>;
+
+struct HuffmanCompressor<T: Clone + Copy + PartialEq + Eq + Hash> {
+    table: TranslationTable<T>
+}
+
+impl<T: Clone + Copy + PartialEq + Eq + Hash> HuffmanCompressor<T> {
+    pub fn add_to_table(table: &mut TranslationTable<T>, tree: &HuffmanNode<T>, path: BitRep) {
+        match tree {
+            HuffmanNode::Leaf(v) => { *table.get_mut(v).unwrap() = path },
+            HuffmanNode::Inner(l, r) => {
+                let left_path = path.push_zero();
+                let right_path = path.push_one();
+                Self::add_to_table(table, l, left_path);
+                Self::add_to_table(table, r, right_path);
             }
-        })
-    );
+        }
+    }
+    pub fn build(tree: &HuffmanNode<T>) -> HuffmanCompressor<T> {
+        let mut table = TranslationTable::new();
+        Self::add_to_table(&mut table, tree, BitRep::new());
+        HuffmanCompressor { table }
+    }
+}
 
-    while queue.len() > 1 {
-        let first = queue.pop().unwrap();
-        let second = queue.pop().unwrap();
-        let combined_freq = first.get_frequency() + second.get_frequency();
-        let new = Node::Inner(Box::new(first), Box::new(second), combined_freq);
-        queue.push(new);
+struct HuffmanDecoder<T: Clone + Copy + PartialEq + Eq + Hash> {
+    arr: Vec<Option<T>>
+}
+
+impl<T: Clone + Copy + PartialEq + Eq + Hash> HuffmanDecoder<T> {
+    pub fn bits_to_idx(b: &BitRep) -> usize {
+        let mut idx = 1;
+        let mut it = b.iter();
+        while let Some(v) = it.next() {
+            idx *= 2;
+            idx += v as usize;
+        }
+        return idx;
     }
 
-    let top_tree: HuffmanNode<T> = queue.pop().unwrap().into();
-    let table = (&top_tree).into();
-    (top_tree, table)
+    pub fn bits_to_idx_consuming<I: Iterator<Item = u8>>(&self, it: &mut I) -> usize {
+        let mut idx = 1;
+        while self.arr[idx].is_none() {
+            idx *= 2;
+            idx += it.next().unwrap() as usize; // if we fail here we've built the tree wrong
+        }
+        return idx;
+    }
+
+    pub fn decode<I: Iterator<Item = u8>>(&self, it: &mut I) -> T {
+        return self.arr[self.bits_to_idx_consuming(it)].unwrap();
+    }
+
+    fn get_values(tree: &HuffmanNode<T>, path: BitRep) -> Vec<(T, BitRep)> {
+        match tree {
+            HuffmanNode::Leaf(v) => vec![(*v, path)],
+            HuffmanNode::Inner(l, r) => {
+                let left_path = path.push_zero();
+                let right_path = path.push_one();
+                let mut v = HuffmanDecoder::get_values(l.as_ref(), left_path);
+                v.append(&mut HuffmanDecoder::get_values(r.as_ref(), right_path));
+                return v; 
+            }
+        }
+    }
+
+    pub fn build_decompressor(tree: &HuffmanNode<T>) -> HuffmanDecoder<T> {
+        let val_list = Self::get_values(tree, BitRep::new());
+        let arr_size = val_list
+            .iter()
+            .map(|v| Self::bits_to_idx(&v.1))
+            .max().unwrap();
+        let mut arr = vec![None; arr_size];
+        for (val, b) in val_list {
+            arr[Self::bits_to_idx(&b)] = Some(val);
+        }
+        HuffmanDecoder { arr }
+    }
 }
+
