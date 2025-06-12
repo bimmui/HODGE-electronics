e01fca916811af3d38fa6fb7940b2d5f9a1b3056
LLai1 <ljcooll236@gmail.com>
2025-06-12 06:04:01 -0500

    it works! (except the magnometer)

--- flight-computer/tests/sd-module-test/sdspi/main/sd_card_example_main.c
+++ flight-computer/tests/sd-module-test/sdspi/main/sd_card_example_main.c
@@ -50,10 +50,10 @@ pin_configuration_t config = {
 
 // Pin assignments can be set in menuconfig, see "SD SPI Example Configuration" menu.
 // You can also change the pin assignments here by changing the following 4 lines.
-#define PIN_NUM_MISO CONFIG_EXAMPLE_PIN_MISO
-#define PIN_NUM_MOSI CONFIG_EXAMPLE_PIN_MOSI
-#define PIN_NUM_CLK CONFIG_EXAMPLE_PIN_CLK
-#define PIN_NUM_CS CONFIG_EXAMPLE_PIN_CS
+#define PIN_NUM_MISO 2
+#define PIN_NUM_MOSI 7
+#define PIN_NUM_CLK 6
+#define PIN_NUM_CS 10
 
 static esp_err_t s_example_write_file(const char *path, char *data)
 {
@@ -94,9 +94,12 @@ static esp_err_t s_example_read_file(const char *path)
 
     return ESP_OK;
 }
-
+    
 void app_main(void)
 {
+    gpio_set_level((gpio_num_t)2, 1);
+    gpio_set_level((gpio_num_t)6, 1);
+    gpio_set_level((gpio_num_t)7, 1);
     esp_err_t ret;
 
     // Options for mounting the filesystem.
@@ -124,7 +127,7 @@ void app_main(void)
     // For setting a specific frequency, use host.max_freq_khz (range 400kHz - 20MHz for SDSPI)
     // Example: for fixed frequency of 10MHz, use host.max_freq_khz = 10000;
     sdmmc_host_t host = SDSPI_HOST_DEFAULT();
-
+    
     // For SoCs where the SD power can be supplied both via an internal or external (e.g. on-board LDO) power supply.
     // When using specific IO pins (which can be used for ultra high-speed SDMMC) to connect to the SD card
     // and the internal LDO power supply, we need to initialize the power supply first.
