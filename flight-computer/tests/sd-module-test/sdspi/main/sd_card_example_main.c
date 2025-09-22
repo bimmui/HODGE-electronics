d73aefd4b7fb4fe9a96533d486e1230ec867dd7c
LLai1 <ljcooll236@gmail.com>
2025-09-22 19:33:31 -0400

    it worked!

--- flight-computer/tests/sd-module-test/sdspi/main/sd_card_example_main.c
+++ flight-computer/tests/sd-module-test/sdspi/main/sd_card_example_main.c
@@ -50,10 +50,10 @@ pin_configuration_t config = {
 
 // Pin assignments can be set in menuconfig, see "SD SPI Example Configuration" menu.
 // You can also change the pin assignments here by changing the following 4 lines.
-#define PIN_NUM_MISO 2
-#define PIN_NUM_MOSI 7
-#define PIN_NUM_CLK 6
-#define PIN_NUM_CS 10
+#define PIN_NUM_MISO 12
+#define PIN_NUM_MOSI 13
+#define PIN_NUM_CLK 14
+#define PIN_NUM_CS 5
 
 static esp_err_t s_example_write_file(const char *path, char *data)
 {
@@ -97,9 +97,9 @@ static esp_err_t s_example_read_file(const char *path)
     
 void app_main(void)
 {
-    gpio_set_level((gpio_num_t)2, 1);
-    gpio_set_level((gpio_num_t)6, 1);
-    gpio_set_level((gpio_num_t)7, 1);
+    gpio_set_level((gpio_num_t)12, 1);
+    gpio_set_level((gpio_num_t)13, 1);
+    gpio_set_level((gpio_num_t)14, 1);
     esp_err_t ret;
 
     // Options for mounting the filesystem.
